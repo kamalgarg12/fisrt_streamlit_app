b06a530be046536ea8b8707048d9a2b34c25e604
kamalgarg12 <kamalgarg790@gmail.com>
2022-12-18 17:19:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 
 import streamlit
 streamlit.title('Hello Kamal')
-streamlit.head('Its a sunny day')
+streamlit.header('Its a sunny day')
 sreamlit.text('come here')
 streamlit.text('can you ellaborate that')
