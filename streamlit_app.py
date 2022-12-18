5bedf99d80f585fc3959a7b8ab8b04b0835304e8
kamalgarg12 <kamalgarg790@gmail.com>
2022-12-18 17:39:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,8 @@ streamlit.title('Hello Kamal')
 streamlit.header('Its a sunny day')
 streamlit.text('come here')
 streamlit.text('can you ellaborate that')
+
+
+import pandas as pd
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
